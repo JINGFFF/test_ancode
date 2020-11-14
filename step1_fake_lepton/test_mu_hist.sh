40d1892aa6208cecba18556581c0478bf366c21c
JINGFFF <15827238926@163.com>
2020-11-14 17:45:47 +0800

    update fake lepton

---
+++ step1_fake_lepton/test_mu_hist.sh
@@ -0,0 +1,11 @@
+#muon channel
+#./preprocess_fake_lepton filelist_dir/tight_muon.txt preprocess_fake_lepton_out/out1 2018 muon data single_muon tight
+#./preprocess_fake_lepton filelist_dir/loose_muon.txt preprocess_fake_lepton_out/out1 2018 muon data single_muon loose
+./preprocess_fake_lepton filelist_dir/tight_DY.txt preprocess_fake_lepton_out/out1 2018 muon mc DY tight
+./preprocess_fake_lepton filelist_dir/loose_DY.txt preprocess_fake_lepton_out/out1 2018 muon mc DY loose
+#./preprocess_fake_lepton filelist_dir/tight_WJets_0j.txt preprocess_fake_lepton_out/out1 2018 muon mc WJets_0j tight
+#./preprocess_fake_lepton filelist_dir/loose_WJets_0j.txt preprocess_fake_lepton_out/out1 2018 muon mc WJets_0j loose
+#./preprocess_fake_lepton filelist_dir/tight_WJets_1j.txt preprocess_fake_lepton_out/out1 2018 muon mc WJets_1j tight
+#./preprocess_fake_lepton filelist_dir/loose_WJets_1j.txt preprocess_fake_lepton_out/out1 2018 muon mc WJets_1j loose
+#./preprocess_fake_lepton filelist_dir/tight_WJets_2j.txt preprocess_fake_lepton_out/out1 2018 muon mc WJets_2j tight
+#./preprocess_fake_lepton filelist_dir/loose_WJets_2j.txt preprocess_fake_lepton_out/out1 2018 muon mc WJets_2j loose
