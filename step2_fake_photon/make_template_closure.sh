a85d19240313e48f69fae36eddef7da26121a8d9
JINGFFF <15827238926@163.com>
2020-11-17 16:12:38 +0800

    update

---
+++ step2_fake_photon/make_template_closure.sh
@@ -0,0 +1,7 @@
+./make_fake_photon_template_closure filelist_dir/tight_WGJets.txt test_closure 2018 mc true WGJets medium 4 10
+./make_fake_photon_template_closure filelist_dir/tight_WJets_0j.txt test_closure 2018 mc fake W0Jets medium 4 10
+./make_fake_photon_template_closure filelist_dir/tight_WJets_1j.txt test_closure 2018 mc fake W1Jets medium 4 10
+./make_fake_photon_template_closure filelist_dir/tight_WJets_2j.txt test_closure 2018 mc fake W2Jets medium 4 10
+./make_fake_photon_template_closure filelist_dir/tight_WJets_0j.txt test_closure 2018 mc real_fake W0Jets medium 4 10
+./make_fake_photon_template_closure filelist_dir/tight_WJets_1j.txt test_closure 2018 mc real_fake W1Jets medium 4 10
+./make_fake_photon_template_closure filelist_dir/tight_WJets_2j.txt test_closure 2018 mc real_fake W2Jets medium 4 10
